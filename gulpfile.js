1afb0799e464dbddcb61b55b3831ffe5eab5ef4b
jwinter2011 <jeremy.winter@selectcomfort.com>
2017-03-24 12:52:56 -0500

    gulp watch
    tasks gulp
    task sasa

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@
 
 var gulp 					= require('gulp');
 var sass 					= require('gulp-sass');
-var browserSync 	= require('browserSync').create();
 
 
 // compile SASS into CSS task
@@ -13,29 +12,16 @@ gulp.task ('sass', function() {
 	return gulp.src('app/scss/**/*.scss') // glob gets any file with .scss
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
     .pipe(gulp.dest('app/css'))
-		.pipe(browserSync.reload({ //Browser Sync injects new CSS styles
-	      stream: true
-	    }))
+		
 });
 
 
 // gulp watch task
-gulp.task ('watch', function(){
-	gulp.task('watch', ['browserSync', 'sass'], function (){
-	  gulp.watch('app/scss/**/*.scss', ['sass']); 
-	  // Other watchers
-	});
+gulp.task('watch', function(){
+  gulp.watch('app/scss/**/*.scss', ['sass']); 
+
 });
 	
-
-// browserSync task
-gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: 'app'
-    },
-  })
-});
 
 
 
