8d279a528d2f0f0e5c83c386432cfcf0151310d7
jwinter2011 <jeremy.winter@selectcomfort.com>
2017-03-24 12:21:43 -0500

    setting up gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,34 +3,39 @@
   ===========
 */
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp 					= require('gulp');
+var sass 					= require('gulp-sass');
+var browserSync 	= require('browserSync').create();
 
 
-/*
-gulp.task('task-name', function() {
-	//gulp task here
-
-});
-*/
-
-
-// compile SASS into CSS
-
+// compile SASS into CSS task
 gulp.task ('sass', function() {
 	return gulp.src('app/scss/**/*.scss') // glob gets any file with .scss
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
     .pipe(gulp.dest('app/css'))
+		.pipe(browserSync.reload({ //Browser Sync injects new CSS styles
+	      stream: true
+	    }))
 });
 
 
-// gulp watch
+// gulp watch task
 gulp.task ('watch', function(){
-  gulp.watch('app/scss/**/*.scss', ['sass']);
-
+	gulp.task('watch', ['browserSync', 'sass'], function (){
+	  gulp.watch('app/scss/**/*.scss', ['sass']); 
+	  // Other watchers
+	});
 });
+	
 
-
+// browserSync task
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+});
 
 
 
