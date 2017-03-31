af6d62578e4ab56e407c9e4c5399568e296ee5f8
jwinter2011 <jeremy.winter@selectcomfort.com>
2017-03-31 13:01:17 -0500

    update

--- gulpfile.js
+++ gulpfile.js
@@ -3,35 +3,18 @@
   ===========
 */
 
-var gulp 	= require('gulp');
-var sass 	= require('gulp-sass');
-//var uglify 	= require('gulp-uglify'); // js minify
+var gulp 		= require('gulp');
+var sass 		= require('gulp-sass');
 var prefix 	= require('gulp-autoprefixer');
-
-
-// function errorLog(error) {
-// 	console.error.bind(error);
-// 	this.emit('end');
-// }
 
 
 // compile SASS into CSS task
 gulp.task ('sass', function() {
 	return gulp.src('app/scss/**/*.scss') // glob gets any file with .scss
 		.pipe(sass()) // Converts Sass to CSS with gulp-sass
-		//on('error', errorLog)
 		.pipe(prefix('last 2 versions'))
 		.pipe(gulp.dest('app/css'));
 });
-
-
-// gulp minify js
-// gulp.task ('uglify', function() {
-// 	return gulp.scr('app/js/*.js')
-// 		.pipe.(uglify())
-// 		//on('error', errorLog)
-// 		.pipe.(gulp.dest('app/minjs'));
-// });
 
 
 // watch task 
